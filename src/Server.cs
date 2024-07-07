f6ddfd8c064a32595396a66a0c9c745ebb103fd2
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2024-07-07 10:38:07 +0200

    Update Server.cs

--- src/Server.cs
+++ src/Server.cs
@@ -8,5 +8,10 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start(); 
-var socket = server.AcceptSocket(); // wait for client
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+var socket = server.AcceptSocket(); // wait for client+
+if (socket.LocalEndPoint.Equals(server.LocalEndpoint))
+{
+    socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+}
+
+socket.Send(Encoding.UTF8.GetBytes("HTTP/1/1 404 Not Found\r\n\r\n"));

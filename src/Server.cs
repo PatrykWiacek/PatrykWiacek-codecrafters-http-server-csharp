96ef54a5c2643b2c5cab4924d2cdbb251ddeff6a
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2024-07-07 11:05:49 +0200

    Update Server.cs

--- src/Server.cs
+++ src/Server.cs
@@ -2,6 +2,9 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+
+string responeOK = "HTTP/1.1 200 OK\r\n\r\n";
+string notFound = "HTTP/1.1 404 Not Found\r\n\r\n";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
@@ -16,6 +19,7 @@ await socket.ReceiveAsync(buffer);
 var request = Encoding.UTF8.GetString(buffer);
 var path = request.Split("\r\n").FirstOrDefault()?.Split(" ")[1];
 
-var response = path is "/" ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+var response = path is "/" ? responeOK : notFound ;
 var bytesRespone = Encoding.UTF8.GetBytes(response);
 socket.Send(bytesRespone);
+

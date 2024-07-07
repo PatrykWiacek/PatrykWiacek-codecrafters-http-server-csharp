99a789296510632d639eb8fb610692f5e9f6380b
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2024-07-07 11:55:13 +0200

    Update Server.cs

--- src/Server.cs
+++ src/Server.cs
@@ -11,7 +11,8 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-
+while (true)
+{
     var socket = await server.AcceptSocketAsync(); // wait for client+
 
     var buffer = new byte[1024];
@@ -22,31 +23,21 @@ server.Start();
     var path = request.Split("\r\n").FirstOrDefault()?.Split(" ")[1];
     if (path == "/")
     {
-        var response = new StringBuilder();
-        response.Append(responseOK);
-        response.Append("Content-Type: text/plain\r\n");
-        response.Append("Content-Length: 0\r\n");
-        response.Append("\r\n");
-
-        var bytesResponse = Encoding.UTF8.GetBytes(response.ToString());
+        string response = $"{responseOK}Content-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
+        var bytesResponse = Encoding.UTF8.GetBytes(response);
         await socket.SendAsync(bytesResponse, SocketFlags.None);
     }
     else if (path.StartsWith("/echo/"))
     {
-        var echoString = path.Substring(6);
+        var echoString = path.Substring(6); // Get the string after /echo/
         
+        // Construct the response
         var responseBody = echoString;
         var contentType = "text/plain";
         var contentLength = Encoding.UTF8.GetByteCount(responseBody).ToString();
 
-        var response = new StringBuilder();
-        response.Append(responseOK);
-        response.Append($"Content-Type: {contentType}\r\n");
-        response.Append($"Content-Length: {contentLength}\r\n");
-        response.Append("\r\n");
-        response.Append(responseBody);
-        
-        var bytesResponse = Encoding.UTF8.GetBytes(response.ToString());
+        string response = $"{responseOK}Content-Type: {contentType}\r\nContent-Length: {contentLength}\r\n\r\n{responseBody}";
+        var bytesResponse = Encoding.UTF8.GetBytes(response);
         await socket.SendAsync(bytesResponse, SocketFlags.None);
     }
     else
@@ -56,6 +47,6 @@ server.Start();
     }
     socket.Shutdown(SocketShutdown.Both);
     socket.Close();
-
+}
 
 
